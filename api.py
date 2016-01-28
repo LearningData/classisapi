e9fa2e3cadf753b30a924edb347f0930b5d80e6a
Marius Avram <marius@learningdata.ie>
2016-01-28 16:21:12 +0000

    moved models to models.py and added connect_db method

--- api.py
+++ api.py
@@ -1,56 +1,12 @@
 import os
+from models import Student, Info, connect_db
 from flask import Flask, redirect, request, jsonify, make_response, abort
-
-from sqlalchemy import create_engine, or_
-from sqlalchemy import Column, Integer, String, ForeignKey
-from sqlalchemy.ext.automap import automap_base
-from sqlalchemy.orm import Session, relationship
+from sqlalchemy import or_, and_
+from sqlalchemy.orm import Session
 
 app = Flask(__name__)
 
-Base = automap_base()
-engine = create_engine(os.environ.get('DB_URL'), convert_unicode=True)
-
-class Student(Base):
-    __tablename__ = 'student'
-
-    id = Column('id', Integer, ForeignKey('info.student_id'), primary_key=True)
-
-    info_id = relationship("Info",
-                        foreign_keys='Info.id',
-                        lazy='subquery',
-                        )
-
-    def active(self):
-        if self.info.enrolstatus == 'C':
-            return True
-        return False
-
-    def json(self):
-        return {
-            'id': self.id,
-            'name': self.forename,
-            'last_name': self.surname,
-            'gender': self.gender,
-            'date_of_birth': str(self.dob),
-            'active': self.active(),
-            'enroll_number': self.info.formerupn,
-            'username': self.info.epfusername,
-            'email': self.info.email,
-            'staff_child': self.info.staffchild,
-            'nationality': self.info.nationality,
-            'entry_date': str(self.info.entrydate),
-            'leaving_date': str(self.info.leavingdate),
-        }
-
-
-class Info(Base):
-    __tablename__ = 'info'
-
-    id = Column('student_id', Integer, ForeignKey('student.id'), primary_key=True)
-
-Base.prepare(engine, reflect=True)
-session = Session(engine)
+db = connect_db(os.environ.get('DB_URL'))
 
 client_id = 'demo'
 
@@ -64,7 +20,7 @@ def api():
 
 @app.route('/api/v2.0/students', methods=['GET'])
 def get_students():
-    students = session.query(Student). \
+    students = db.query(Student). \
             join(Student.info). \
             filter(or_(Info.enrolstatus == 'C', Info.enrolstatus == 'P')). \
             all()
@@ -77,7 +33,7 @@ def get_students():
 
 @app.route('/api/v2.0/students/<int:student_id>', methods=['GET'])
 def get_student(student_id):
-    student = session.query(Student). \
+    student = db.query(Student). \
             filter(Student.id == student_id). \
             first()
 
