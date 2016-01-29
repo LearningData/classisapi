6c2091b31b0987635655c7625df4e942ff6a729a
Marius Avram <marius@learningdata.ie>
2016-01-29 16:17:41 +0000

    added classes queries by parameters

--- api.py
+++ api.py
@@ -1,5 +1,5 @@
 import os
-from models import Student, Info, Teacher, Guardian, Class, connect_db
+from models import Student, Info, Teacher, Guardian, Cohort, Class, connect_db
 from flask import Flask, redirect, request, jsonify, make_response, abort
 from sqlalchemy import or_, and_
 from sqlalchemy.orm import Session
@@ -107,8 +107,49 @@ def get_classes():
             all()
 
     return jsonify({
-        'client_id': client_id,
-        'count': len(classes),
+        '_client_id': client_id,
+        '_count': len(classes),
+        'classes': [teaching_class.json() for teaching_class in classes]
+        })
+
+@app.route('/api/v2.0/classes/<int:class_id>', methods=['GET'])
+def get_class(class_id):
+    teaching_class = db.query(Class). \
+            filter(Class.id == class_id). \
+            first()
+
+    if not teaching_class:
+        abort(404)
+
+    return jsonify({
+        '_client_id': client_id,
+        '_count': 1,
+        'classes': teaching_class.json()
+        })
+
+@app.route('/api/v2.0/classes/year/<int:year>', methods=['GET'])
+def get_class_by_year(year):
+    classes = db.query(Class). \
+            join(Class.cohort).\
+            filter(Cohort.year == year). \
+            all()
+
+    return jsonify({
+        '_client_id': client_id,
+        '_count': len(classes),
+        'classes': [teaching_class.json() for teaching_class in classes]
+        })
+
+@app.route('/api/v2.0/classes/course/<course>', methods=['GET'])
+def get_class_by_course(course):
+    classes = db.query(Class). \
+            join(Class.cohort).\
+            filter(Cohort.course_id == course). \
+            all()
+
+    return jsonify({
+        '_client_id': client_id,
+        '_count': len(classes),
         'classes': [teaching_class.json() for teaching_class in classes]
         })
 
