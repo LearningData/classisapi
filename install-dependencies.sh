43cb4d5e51f1fd480cb911d85f42f031fadbcbe4
Marius Avram <mariusavram91@gmail.com>
2016-02-02 18:35:47 +0000

    Update install-dependencies.sh

--- install-dependencies.sh
+++ install-dependencies.sh
@@ -3,8 +3,3 @@ apt-get update
 apt-get -y install python-pip python-dev build-essential
 apt-get -y install libmysqlclient-dev
 pip install virtualenvwrapper
-export WORKON_HOME=$HOME/.virtualenvs
-export PROJECT_HOME=$HOME/Devel
-source /usr/local/bin/virtualenvwrapper.sh
-mkvirtualenv classisapi
-workon classisapi
